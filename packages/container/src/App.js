12a8c1770ae2489a530a0e5d1153099265faeedc
Subin Fernandez <subinfernandez@gmail.com>
2021-08-01 03:10:26 +0100

    Added dashboard sub app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,23 +1,33 @@
-import React, { lazy, Suspense, useState } from 'react';
-import { BrowserRouter, Route, Switch  } from 'react-router-dom';
+import React, { lazy, Suspense, useState, useEffect } from 'react';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+import { createBrowserHistory } from 'history';
 
 import Header from './components/Header';
 import Progress from './components/Progress';
 
 const MarketingLazy = lazy(() => import('./components/MarketingApp'));
 const AuthLazy = lazy(() => import('./components/AuthApp'));
+const DashboardLazy = lazy(() => import('./components/DashboardApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
 });
 
+const history = createBrowserHistory();
+
 export default () => {
   const [isSignedin, setIsSignedIn] = useState(false);
 
+  useEffect(() => {
+    if (isSignedin) {
+      history.push('/dashboard');
+    }
+  }, [isSignedin]);
+
   return (
     <StylesProvider generateClassName={generateClassName}>
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header onSignOut={() => setIsSignedIn(false)} isSignedIn={isSignedin} />
           <Suspense fallback={<Progress />}>
@@ -25,13 +35,17 @@ export default () => {
               <Route path="/auth">
                 <AuthLazy onSignIn={() => setIsSignedIn(true)} />
               </Route>
+              <Route path="/dashboard">
+                {!isSignedin && <Redirect to="/" />}
+                <DashboardLazy />
+              </Route>
               <Route path="/">
                 <MarketingLazy />
               </Route>
             </Switch>
           </Suspense>
         </div>
-      </BrowserRouter>
+      </Router>
     </StylesProvider>
   );
 };
